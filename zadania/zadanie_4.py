9492f78557f6e86b0929104e9728e9d45ae0cbb3
melisa-qordoba <melisa@qordoba.com>
2020-09-12 17:14:33 +0200

    zadania 4 i 5

---
+++ zadania/zadanie_4.py
@@ -0,0 +1,6 @@
+"""
+Zad.4 (met. < for loop >):
+"""
+
+for x in range(0, 10):
+    print("hello, I'm " + str(x) + "!")
---
+++ zadania/zadanie_5.py
@@ -0,0 +1,14 @@
+"""
+Zad.5 (met. < print >):
+"""
+
+print("Hello, I'm 0!")
+print("Hello, I'm 1!")
+print("Hello, I'm 2!")
+print("Hello, I'm 3!")
+print("Hello, I'm 4!")
+print("Hello, I'm 5!")
+print("Hello, I'm 6!")
+print("Hello, I'm 7!")
+print("Hello, I'm 8!")
+print("Hello, I'm 9!")
